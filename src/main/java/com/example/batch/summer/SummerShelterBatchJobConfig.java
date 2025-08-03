c2fdd679fd27e622f78ffa2e6447af2c3457e09b
stophyeon <jj6778@naver.com>
2025-08-03 15:16:17 +0900

    배치 동작시 로그 설정

--- src/main/java/com/example/batch/summer/SummerShelterBatchJobConfig.java
+++ src/main/java/com/example/batch/summer/SummerShelterBatchJobConfig.java
@@ -1,6 +1,7 @@
 package com.example.batch.summer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -8,9 +9,12 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 
+@Slf4j
 @Configuration
+@EnableScheduling
 @RequiredArgsConstructor
 public class SummerShelterBatchJobConfig {
 
@@ -21,6 +25,7 @@ public class SummerShelterBatchJobConfig {
 
     @Bean
     public Job summerShelterJob(JobRepository jobRepository, PlatformTransactionManager txManager) {
+        log.error("Get Summer Shelter Data Scheduler run");
         return new JobBuilder("summerShelterJob", jobRepository)
                 .start(summerShelterStep(jobRepository, txManager))
                 .build();
